1462594e58472a16263db0aab645bfb79ffc2f6f
Thanga Ganapathy <ganapathy888@gmail.com>
2026-07-16 03:48:23 +0530

    docs: restructure docs to multi-page format and use MDX Tabs

--- website/app/docs/_meta.js
+++ website/app/docs/_meta.js
@@ -2,4 +2,7 @@
 // control their order in the generated navigation.
 export default {
   index: "Getting Started",
+  api: "API Reference",
+  examples: "Examples",
+  caveats: "Caveats & FAQs",
 };
